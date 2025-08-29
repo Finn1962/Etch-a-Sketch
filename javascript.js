34fe1bf7a71875cc4a35283c5f5c33270c756901
Finn Schmidt <finn.schmidt@brainworxx.de>
2025-08-29 15:19:41 +0200

    Erstellung des Rasters funktioniert

--- javascript.js
+++ javascript.js
@@ -1,23 +1,30 @@
-document.addEventListener("DOMContentLoaded", function erstellungZeichenfläche(){
+document.addEventListener("DOMContentLoaded", function erstellungZeichenflaeche(){
     
+    //Erstellung der Zeichenfläche durch verschachtelten Arrays
+    const zeichenflaecheBreite_X = (5);
+    const zeichenflaeche = document.getElementById("zeichenflaeche");
+    const zeichenfleacheBreiteInPixel = (zeichenflaeche.clientWidth)
 
-    //Erstellung der Zeichenfläche mit verschachtelten Arrays
-    let breiteZeichenfläche_X = (5);
-    let zeichenfläche_Y = [];
-    
-    for (let y = 0; y < breiteZeichenfläche_X; y++) {
-    
-        zeichenfläche_Y.push(zeichenfläche_X = []);
 
-        for (let x = 0; x < breiteZeichenfläche_X; x++) {
+    const zeichenflaecheSpaltenLaenge_Y = [];
+ 
+    for (let y = 0; y < zeichenflaecheBreite_X; y++) {
+        
+        const zeichenflaecheZeilenBreite_X = [];
+        zeichenflaecheSpaltenLaenge_Y.push(zeichenflaecheZeilenBreite_X);
 
-            zeichenfläche_X.push(0)            
+        const neueZeile = document.createElement("p");
+        zeichenflaeche.appendChild(neueZeile);
 
+        for (let x = 0; x < zeichenflaecheBreite_X; x++) {
+
+            const pixel = document.createElement("div");
+            pixel.id = "("+(y)+"/"+(x)+")";
+            pixel.classList.add('pixelClasse');
+            const pixelGroesse = (zeichenfleacheBreiteInPixel / zeichenflaecheBreite_X);
+            pixel.style.width = pixelGroesse + "px";
+            pixel.style.height = pixelGroesse + "px";
+            neueZeile.appendChild(pixel);           
         }
-    
-    
     }  
-    console.log(zeichenfläche_Y);
-
-    
 });
